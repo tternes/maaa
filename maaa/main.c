09dcdc683d5199540809ecc9317ef284da91b3f6
Thaddeus Ternes <t.ternes@c02j6029dkq4.hq.sococo.net>
2013-07-01 14:26:31 -0500

    Initial Commit

---
+++ maaa/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  maaa
+//
+//  Created by Thaddeus Ternes on 7/1/13.
+//  Copyright (c) 2013 Bluetoo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
