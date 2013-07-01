8137b3259449dfdde6efc5be3717a20f5b1ffe9e
Thaddeus Ternes <thaddeus@bluetoo.co>
2013-07-01 15:09:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+maaa: memory allocator and abuser
+
+maaa is a trivial memory allocation utility for helping test memory thresholds.
+
+Usage: maaa <MB to be allocated>
--- maaa/main.c
+++ maaa/main.c
@@ -7,12 +7,42 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    printf("Hello, World!\n");
+    if(argc < 2)
+    {
+        fprintf(stderr, "Usage: maaa <MB>\n");
+        return -1;
+    }
+
+    const unsigned long ulMB = 1024 * 1024;
+    unsigned long ulAllocationMegabytes = strtoul(argv[1], NULL, 10);
+    
+    fprintf(stdout, "Allocating %lu MB...\n", ulAllocationMegabytes);
+    
+    void *buffer = malloc(ulAllocationMegabytes * 1024 * 1024);
+
+    fprintf(stdout, "Starting write cycle, press ctrl-c to stop\n");
+    for(int pass=0; ; pass++)
+    {
+        for(unsigned long index = 0; index < ulAllocationMegabytes; index++)
+        {
+            memset(buffer + (index * ulMB), pass, ulMB);
+            fprintf(stdout, "\rPass %i MB block %li", pass, index);
+            usleep(10000);
+            fflush(stdout);
+        }
+        
+        fprintf(stdout, " done\n");
+        sleep(1);
+    }
+    
     return 0;
 }
 
